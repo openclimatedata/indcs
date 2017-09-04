fb80ee4ab1e0c3bb19a7103933d547f83bea8e0b
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2017-09-04 15:32:04 +0200

    Remove license header block

--- scripts/process.py
+++ scripts/process.py
@@ -1,14 +1,5 @@
 # encoding: UTF-8
 
-# Written in 2016 - 2017 by Robert Gieseke <robert.gieseke@pik-potsdam.de>
-# To the extent possible under law, the author(s) have dedicated all copyright
-# and related and neighboring rights to this software to the public domain
-# worldwide. This software is distributed without any warranty.
-#
-# You should have received a copy of the CC0 Public Domain Dedication along
-# with this software. If not, see
-# <http://creativecommons.org/publicdomain/zero/1.0/>.
-
 import os
 import re
 import urllib.request
